45c3ba79a91bd31215f4c2f0d9dd578b311f6f32
Pedro Henrique Coterli <pedrocoterli@gmail.com>
2025-06-17 11:38:08 -0300

    Main ajustada

--- pipeline_functions.py
+++ pipeline_functions.py
@@ -1,7 +1,45 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, sum, month, to_timestamp
-from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType, TimestampType
 
 def join(df1, df2, id):
     df_joined = df1.join(df2, on = id)
     return df_joined
+
+def groupby_stats_hotels(df_joined_hotel):
+    df_with_month = df_joined_hotel.withColumn(
+        "mes_reserva", month(col("data_reserva"))
+    )
+    
+    df_grouped_stats = df_with_month.groupBy(
+        "company_id",
+        "cidade",
+        "mes_reserva"
+    ).agg(
+        sum("valor").alias("sum_valor")
+    )
+
+    return df_grouped_stats
+
+def groupby_city_hotels(df_stats_hotels):
+    df_grouped_city = df_stats_hotels.groupBy(
+        "company_id",
+        "cidade",
+    ).agg(
+        sum("sum_valor").alias("sum_valor")
+    ).orderBy(
+        "company_id", "sum_valor"
+    )
+
+    return df_grouped_city
+
+def groupby_month_hotels(df_stats_hotels):
+    df_grouped_month = df_stats_hotels.groupBy(
+        "company_id",
+        "mes_reserva",
+    ).agg(
+        sum("sum_valor").alias("sum_valor")
+    ).orderBy(
+        "company_id", "sum_valor"
+    )
+
+    return df_grouped_month
